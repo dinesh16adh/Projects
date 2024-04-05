a4691106a78afa51453cdf71bb83e8224cb6bb01
unknown <dinesh683adhikari@gmail.com>
2024-04-05 15:32:59 +0545

    add

--- instabot.py
+++ instabot.py
@@ -2,4 +2,6 @@ from instabot import Bot
 
 bot = Bot()
 bot.login(username="d__ness_04", password="npmrunstart12@")
-bot.follow("sidd_ghimire")
+# bot.follow("sidd_ghimire")
+# bot.upload_photo()
+bot.sentmessage("I love python", ["krish__na077"])
